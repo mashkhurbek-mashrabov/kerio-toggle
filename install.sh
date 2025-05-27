3522a775c41a4b445f02c6359202754d474b5ffa
Mashhurbek <mashkhurbek.mashrabov@gmail.com>
2025-05-27 17:28:15 +0500

    fix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -r ~/.local/share/gnome-shell/extensions/kerio-toggle@mashkhurbek
-cp -r ./kerio-toggle@mashkhurbek\ / ~/.local/share/gnome-shell/extensions/kerio-toggle@mashkhurbek
+cp -r ./kerio-toggle@mashkhurbek ~/.local/share/gnome-shell/extensions/kerio-toggle@mashkhurbek
